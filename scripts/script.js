653b91ed79bc25b487e6ff27c36cffe480892fc1
Adelia <adelgants@gmail.com>
2020-07-18 05:53:53 +0300

    Updates

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,10 @@ let themeBtn = document.querySelector('.header__button');
 themeBtn.addEventListener("click", function() {
   if(document.documentElement.hasAttribute("theme")){
     document.documentElement.removeAttribute("theme");
+    themeBtn.classList.add('switch-on');
+
   } else {
     document.documentElement.setAttribute("theme", "dark");
+    themeBtn.classList.remove('switch-on');
   }
 });
